720d854ad1d0b158de7ffe89d109b78bee7beeec
devUserContact <developer.user.contact@protonmail.com>
2022-09-21 21:21:55 -0700

    added component that formats posts.body

--- components/PostBodyFormatted/index.tsx
+++ components/PostBodyFormatted/index.tsx
@@ -1,22 +1,25 @@
-import React from "react";
-import Markdown from "react-markdown";
-import { readline }from "linebyline";
+import React from 'react';
+import Markdown from 'react-markdown';
 
 const PostBodyFormatted = ({ post }: any) => {
-	let media =  JSON.parse(post.media)
-	if (typeof media.media.imgur === 'undefined') {
-		return (
-			<Markdown linkTarget={"_black"}>
-				{post.body}
-			</Markdown>
-		)
-	} else {
-		let rl = readline(post.body);
-		rl.on('line', function(line, lineCount) {
-			console.log(line)
-		})
-	}
-	
+  let media = JSON.parse(post.media);
+  if (typeof media.media.imgur === 'undefined') {
+    return <Markdown linkTarget={'_black'}>{post.body}</Markdown>;
+  } else {
+    let imgurLinks = [];
+    media.media.imgur.map((link, index) => {
+      imgurLinks.push(link);
+    });
+    let bodyArray = post.body.split(/(IMGUR)/);
+    return bodyArray.map((fragment, index) => {
+      if (fragment != `IMGUR`) {
+        return <Markdown>{fragment}</Markdown>;
+      }
+      if (fragment === `IMGUR`) {
+        return <img src={`${imgurLinks}`} />;
+      }
+    });
+  }
 };
 
 export default PostBodyFormatted;
