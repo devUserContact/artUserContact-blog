c7036b19f36c7ec5c0bc2e9f4bd3d5b4a7186ed5
devUserContact <developer.user.contact@protonmail.com>
2022-09-26 13:41:00 -0700

    edited README and whoAmI

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # [artUserContact-blog][1]
-[![Netlify
-Status](https://api.netlify.com/api/v1/badges/836d9476-3f42-4715-9eca-05f81e94eb17/deploy-status)](https://app.netlify.com/sites/artusercontact-blog/deploys)
-
+[![Netlify Status](https://api.netlify.com/api/v1/badges/836d9476-3f42-4715-9eca-05f81e94eb17/deploy-status)](https://app.netlify.com/sites/artusercontact-blog/deploys)
 ---
 
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
--- assets/Whoami/index.ts
+++ assets/Whoami/index.ts
@@ -1,12 +1,14 @@
-export const Whoami: any = `## greetings all, 
+export const Whoami: any = `## hello, 
 ### i hope you are having a great day.
 ### i go by _maxwell_.
 ___
 this blog is where i post some of my art as well as work on new features for my
 blog network. i hope that you enjoy your time here- however long. 
 
-if there is anything you see that you like, please feel free to contact me for 
+if you see anything that you like, please feel free to contact me with 
 inquires or commissions. 
 
-artUserContact
+[email][email]
+
+[email]:mailto:art.user.contact@protonamil.com "art.user.contact@protonmail" 
 `;
